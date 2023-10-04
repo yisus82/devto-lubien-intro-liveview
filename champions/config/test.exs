090b965226f77c19ab34144a2a062f5d92523f76
yisus82 <djalma8@gmail.com>
2023-10-04 10:01:29 +0200

    Adding a points system

--- champions/config/test.exs
+++ champions/config/test.exs
@@ -9,8 +9,8 @@ config :bcrypt_elixir, :log_rounds, 1
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :champions, Champions.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "yisus82",
+  password: "",
   hostname: "localhost",
   database: "champions_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
